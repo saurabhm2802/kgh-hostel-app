2c092752d1ba82918a87502798a8760789da14a9
saurabhm2802 <saurabhm2802@gmail.com>
2026-09-18 16:47:29 +0530

    Update RoomRepository.kt

--- app/src/main/java/com/kgh/hostel/data/repository/RoomRepository.kt
+++ app/src/main/java/com/kgh/hostel/data/repository/RoomRepository.kt
@@ -14,12 +14,12 @@ class RoomRepository @Inject constructor(
     fun observeRooms() = dao.observeRooms()
     fun observeBedsForRoom(roomId: Long) = dao.observeBedsForRoom(roomId)
     fun observeVacantBeds() = dao.observeVacantBeds()
+    fun observeAllBeds() = dao.observeAllBeds()
     fun observeTotalBeds() = dao.observeTotalBeds()
     fun observeOccupiedBeds() = dao.observeOccupiedBeds()
     fun observeVacantBedCount() = dao.observeVacantBedCount()
     fun observeHistoryForStudent(studentId: Long) = dao.observeHistoryForStudent(studentId)
 
-    /** Creates a room and immediately generates its N beds (Bed 1..N). */
     suspend fun addRoomWithBeds(room: HostelRoom): Long {
         val roomId = dao.insertRoom(room)
         for (i in 1..room.totalBeds) {
