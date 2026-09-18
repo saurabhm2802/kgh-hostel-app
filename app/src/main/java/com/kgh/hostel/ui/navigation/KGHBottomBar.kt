853985caa140c9047ba3e2fb09c7e5c7826cdc9c
saurabhm2802 <saurabhm2802@gmail.com>
2026-09-18 12:22:37 +0530

    Update KGHBottomBar.kt

--- app/src/main/java/com/kgh/hostel/ui/navigation/KGHBottomBar.kt
+++ app/src/main/java/com/kgh/hostel/ui/navigation/KGHBottomBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
